13cc64688855d66536e6379d6e525eab2a9ed054
tinyloli <allison.dimailig.854@my.csun.edu>
2017-04-04 16:58:21 -0700

    coffee -> pizza

--- functions.php
+++ functions.php
@@ -10,23 +10,23 @@ function capfirst($word) {
   return $word;
 }
 
-function makeCoffee($theName, $theDrinks, $theCoffee, $theQuantity) {
-  if ($theCoffee != 'nothing' && $theQuantity > 0) {
+function makePizza($theName, $theFood, $thePizza, $theQuantity) {
+  if ($thePizza != 'nothing' && $theQuantity > 0) {
     $valid = true;
-    $price = $theDrinks[$theCoffee];
+    $price = $theFood[$thePizza];
     $total = $price * $theQuantity;
     if ($theQuantity < 2) {
-      $title = titlecase($theCoffee).' for '.$theName;
+      $title = titlecase($thePizza).' for '.$theName;
       $theTotal = 'Total: $'.number_format($total, 2);
-      $description = $theName.' ordered '.$theQuantity.' '.$theCoffee.'.';
+      $description = $theName.' ordered '.$theQuantity.' '.$thePizza.'.';
     } elseif ($theQuantity > 50) {
-      $title = 'No '.titlecase($theCoffee).' for '.$theName;
-      $theCoffee = 'rediculous';
-      $description = 'Don&rsquo;t be rediculous, '.$theName.', that&rsquo;s more coffee than any one barista can ever brew on time! Also, you don&rsquo;t have $'.number_format($total, 2).'!';
+      $title = 'No '.titlecase($thePizza).' for '.$theName;
+      $thePizza = 'ridiculous';
+      $description = 'Don&rsquo;t be ridiculous, '.$theName.', that&rsquo;s more pizza than any one chef can ever bake on time! Also, you don&rsquo;t have $'.number_format($total, 2).'!';
     } else {
-      $title = titlecase($theCoffee).' for '.$theName;
+      $title = titlecase($thePizza).' for '.$theName;
       $theTotal = 'Total: $'.number_format($total, 2);
-      $description = $theName.' ordered '.number_format($theQuantity).' '.$theCoffee.'s.';
+      $description = $theName.' ordered '.number_format($theQuantity).' '.$thePizza.'s.';
     }
   } else {
     $valid = false;
@@ -35,7 +35,7 @@ function makeCoffee($theName, $theDrinks, $theCoffee, $theQuantity) {
   if ($valid == true) {
     return('
       <div class="card my-4 mx-auto" style="width: 20rem;">
-        <img class="img-fluid" src="images/'.$theCoffee.'.jpg" alt="Card image cap">
+        <img class="img-fluid" src="images/'.$thePizza.'.jpg" alt="Card image cap">
         <div class="card-block">
           <h2 class="h4 card-title">'.$title.'</h2>
           <p class="card-text">'.$description.'</p>
@@ -49,7 +49,7 @@ function makeCoffee($theName, $theDrinks, $theCoffee, $theQuantity) {
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
-        <p class="m-0"><strong>Oops!</strong> Pick a drink.</p>
+        <p class="m-0"><strong>Oops!</strong> Pick a pizza.</p>
       </div>
     ');
   }
--- index.php
+++ index.php
@@ -3,25 +3,25 @@
   require_once('functions.php');
 
   // Create coffee array with prices
-  $theDrinks = array(
-    'coffee'=>1.25,
-    'espresso'=>2.5,
-    'latte'=>4.25,
-    'mocha'=>4.75
+  $theFood = array(
+    'pizza'=>4,
+    'cheese pizza'=>5.5,
+    'hawaiian pizza'=>4.25,
+    'vegetarian pizza'=>4.75
   );
 
   // Process the form request
   if( isset($_POST['submit']) )
   {
       $theName = htmlentities($_POST['name']);
-      $theCoffee = htmlentities($_POST['coffee']);
-      $theCoffee = strtolower($theCoffee); // Lowercase form submission
+      $thePizza = htmlentities($_POST['pizza']);
+      $thePizza = strtolower($thePizza); // Lowercase form submission
       $theQuantity = htmlentities($_POST['quantity']);
-      $coffee = makeCoffee($theName, $theDrinks, $theCoffee, $theQuantity);
+      $pizza = makePizza($theName, $theFood, $thePizza, $theQuantity);
   } else {
     // User hasn't entered a value
-    $theCoffee = '';
-    $coffee = '';
+    $thePizza = '';
+    $pizza = '';
     $theName = '';
   }
 ?>
@@ -30,27 +30,27 @@
 <html lang="en">
   <head>
     <meta charset="UTF-8">
-    <title>Functional Coffee</title>
+    <title>Functional Pizza</title>
     <link rel="stylesheet" href="css/bootstrap.css">
   </head>
   <body class="bg-faded">
     <main class="container py-4">
-      <h1 class="pb-4 font-weight-bold text-center">What Will You Drink?</h1>
+      <h1 class="pb-4 font-weight-bold text-center">What Will You Eat?</h1>
       <form class="form-inline justify-content-center" action="" method="post">
         <label for="name" class="sr-only">Name</label>
         <input class="form-control mr-2" type="text" value="<?php echo ( $theName ? $theName : '' );?>" placeholder="Name" name="name" id="name"> <span class="mr-2">wants a</span>
-        <select class="custom-select mr-2" name="coffee" id="coffee">
-          <?php echo '<option value="'.( $theCoffee ? $theCoffee : 'nothing' ).'">'.( $theCoffee ? capfirst($theCoffee) : 'Select a Drink' ).'</option>'; ?>
-          <?php foreach ($theDrinks as $drink => $price) {
-            echo '<option value="'.$drink.'">'.capfirst($drink).'</option>';
+        <select class="custom-select mr-2" name="pizza" id="pizza">
+          <?php echo '<option value="'.( $thePizza ? $thePizza : 'nothing' ).'">'.( $thePizza ? capfirst($thePizza) : 'Select a Pizza' ).'</option>'; ?>
+          <?php foreach ($theFood as $food => $price) {
+            echo '<option value="'.$food.'">'.capfirst($food).'</option>';
           }; ?>
         </select> <span class="mr-2">for</span>
         <input class="form-control mr-2" type="number" value="<?php echo ( $theQuantity ? $theQuantity : '' );?>" placeholder="Quantity" name="quantity" id="quantity">
         <button class="btn btn-outline-primary" name="submit" type="submit">Submit</button>
       </form>
       <?php 
-        if($coffee){
-           echo $coffee;
+        if($pizza){
+           echo $pizza;
         }
       ?>
     </main>
